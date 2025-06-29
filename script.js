02840acbc297a5d2461f003c59fa9753b8832555
AlexandreMello-dev <122244229+AlexandreMello-dev@users.noreply.github.com>
2025-06-29 18:47:44 -0300

    Teste

---
+++ script.js
@@ -0,0 +1,13 @@
+var num1 = 3
+var num2 = 5
+
+function sumRange(num1, num2){
+    for(let i = num1; i <= num2; i++){
+        for(let j = 1; j <= num2; j++){
+            let soma = i + j
+        }
+    }
+    console.log(soma)
+}
+
+sumRange(num1, num2)
